6a54c47e09de1b0b0b50cf1b14d9f33c54b79912
octy40 <octy40@gmail.com>
2015-08-12 19:07:57 -0400

    New file to process google's custom search api

---
+++ project_dmitri2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 19:43:46 2015
+
+@author: muhirwao
+"""
+
+# Import modules that will be used
+
+import json
+import requests
+
+# Declare fixed variables
+
+mined_urls = []
+key = "AIzaSyDttxqs-uahYWQa2kefZRotb_iAF9jCyW4"
+cx = "008280251192147562951:rghr1k2p058"
+url = "https://www.googleapis.com/customsearch/v1"
+parameters = {"q": "banana",
+              "cx": cx,
+              "key": key,
+              "searchType": "image",
+              "start": results["queries"]["nextPage"][0]["startIndex"],
+              }
+
+# Use requests to query the API website and assign results to a variable
+page = requests.request("GET", url, params=parameters)
+results = json.loads(page.text)
+
+
+
+print results["searchInformation"]
